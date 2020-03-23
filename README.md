1d924d57b8172e02bece80aad3f3197d55280e59
ArqamRafay <getarqamnow@hotmail.com>
2020-03-23 15:21:43 +0500

    final reshape

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # openCVLibrary-
+Goal
+- Learn to read video, display video, and save video.
+- Learn to capture video from a camera and display it.
+- You will learn these functions : cv.VideoCapture(), cv.VideoWriter()
+
+# Capture Video from Camera
+Often, we have to capture live stream with a camera. OpenCV provides a very simple interface to do this. Let's capture a video from the camera (I am using the built-in webcam on my laptop), convert it into grayscale video and display it. Just a simple task to get started.
--- drawGeometricShap.py
+++ drawGeometricShap.py
@@ -3,9 +3,14 @@ import cv2
 
 img = cv2.imread('car.jpg', 1)
 
-img = cv2.arrowedLine(img, (0,0), (255,255), (255,0,0), 5)
-img = cv2.line(img, (0,255), (255,255), (255,0,0), 5)
-
+img = cv2.line(img, (0,0), (255,255), (147,96,44), 5)
+img = cv2.arrowedLine(img, (0,255), (255,255), (255,0,0), 5)
+img = cv2.rectangle(img , (10,10), (100,100), (0,0,255),5)
+img = cv2.circle(img,(447,63),63, (0,0,255), 2)
+# -1 indecate the fill color of circle
+# img = cv2.circle(img,(447,63),63, (0,0,255), -1)
+font = cv2.FONT_HERSHEY_SIMPLEX
+img = cv2.putText(img, 'Open Cv ', (10,500), font,4, (255,255,255),10, cv2.LINE_AA)
 
 cv2.imshow('imag',img)
 cv2.waitKey(0)
