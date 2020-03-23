56cc296e75633503273eac084dc072cf6a255c0c
ArqamRafay <getarqamnow@hotmail.com>
2020-03-23 16:12:07 +0500

    new lecture partical

---
+++ addTextToVideo.py
@@ -0,0 +1,24 @@
+import cv2
+cap = cv2.VideoCapture(0)
+print(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+print(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+# USing set method use camera width height
+cap.set(3,600)
+cap.set(4,600)
+print(cap.get(3))
+print(cap.get(4))
+
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    if ret == True:
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        cv2.imshow('frame', gray)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
